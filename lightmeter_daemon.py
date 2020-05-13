8b9d84b5248ec37d919903d6864af927453ade72
Jörg Weingrill <jweingrill@gmail.com>
2020-05-13 19:38:25 +0000

    removed logfiles; added lightmeter_daemon; conversion to watts

---
+++ lightmeter_daemon.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import daemon
+import logging
+from time import sleep
+from lightmeter import Lightmeter
+
+logging.basicConfig(filename='lightmeter.log',level=logging.DEBUG)
+
+def main_program():
+    logging.debug('main_program()')
+    try:
+        logging.info('connecting lightmeter')
+        lmeter = Lightmeter()
+    except usb.USBError as e:
+        if e.errno != 13:
+                raise e
+        logging.exception(e, file=sys.stderr)
+        logging.warning('Set read/write permissions on device node '
+            '/dev/bus/usb/{:03d}/{:03d}'.format(e.bus,e.address),
+            file=sys.stderr)
+        logging.warning('Alternatively, use udev to fix this permanently.')
+        exit(1)
+    logging.debug('connecting database')
+    lmeter.connect_db()
+#    killer = GracefulKiller()
+
+#    while not killer.kill_now:
+    while True:
+        logging.debug('reading lightmeter')
+        l = lmeter.read()
+        logging.debug('writing database')
+        lmeter.write_database(l)
+        logging.debug('waiting for next cycle')
+        sleep(0.5)
+
+
+if __name__ == '__main__':
+    logging.info('Starting daemon…')
+    stdin  = open('/dev/null', 'rb')
+    stdout = open('lightmeter.info', 'w+b')
+    stderr = open('lightmeter.err', 'w+b', buffering=0)
+    context = daemon.DaemonContext(uid=1000,
+                                   gid=1000,
+                                   stdin=stdin,
+                                   stdout=stdout,
+                                   stderr=stderr,
+                                   detach_process=True)
+    with context:
+        logging.debug('in context')
+        main_program()
+    stdin.close()
+    stdout.close()
+    stderr.close()
+    logging.info('leaving context')
