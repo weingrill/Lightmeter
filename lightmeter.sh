c94102d405ec0896e2c4341f6db62eb331a2c0a3
Jörg Weingrill <jweingrill@gmail.com>
2020-11-29 21:35:21 +0000

    updated lightmeter.sh for crontab and grep process check

--- lightmeter.sh
+++ lightmeter.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-cd /home/debian/Kuffner-Sternwarte-Lightmeter
-source venv/bin/activate
-python3 lightmeter_daemon.py
-deactivate
+if pgrep -x "python3" > /dev/null; then
+    echo "running"
+else
+    cd /home/debian/Kuffner-Sternwarte-Lightmeter
+    source venv/bin/activate
+    python3 lightmeter_daemon.py
+    deactivate
+fi
 
 if [ -s lightmeter.err ]; then
     mv -f lightmeter.err lightmeter.err.bak
     touch lightmeter.err
-    echo file is not zero size, reboot commencing
-#    sleep 60
-#    /sbin/reboot
 else
     echo no error detected
 fi
